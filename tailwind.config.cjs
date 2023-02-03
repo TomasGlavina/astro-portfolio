fdfff1f42820dd0619dddc36f7d8d430ac230839
Tomas <tomi.glavina@gmail.com>
2023-02-03 14:38:09 +0200

    added a head layout, font and started navbar

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,14 @@
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		fontFamily: {
+			"sans": ["Ubuntu", "sans-serif"],
+			"mono": ["Ubuntu Mono", "monospace"],
+		},
+		extend: { },
+
 	},
-	plugins: [],
+	plugins: [
+		require('daisyui'),
+	],
 }
