00d1c0269323b1bc11b61fc3c7b6201100a3ac47
Tomas <tomi.glavina@gmail.com>
2023-02-04 11:39:30 +0200

    added navbar

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,15 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		fontFamily: {
-			"sans": ["Ubuntu", "sans-serif"],
-			"mono": ["Ubuntu Mono", "monospace"],
-		},
-		extend: { },
-
-	},
-	plugins: [
-		require('daisyui'),
-	],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  darkMode: 'class',
+  theme: {
+    fontFamily: {
+      sans: ["Ubuntu", "sans-serif"],
+      mono: ["Ubuntu Mono", "monospace"],
+    },
+    extend: {},
+  },
+  plugins: [require("daisyui")],
+};
